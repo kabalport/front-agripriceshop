d03c35f822db26ac7cec3e379e054d1c1097285e
최대열 <dychoi@brainednet.com>
2023-06-05 09:42:23 +0900

    자유게시판레이아웃작업중

--- src/components/pages/CommunityPage.tsx
+++ src/components/pages/CommunityPage.tsx
@@ -1,15 +1,64 @@
-// src/components/pages/CommunityPage.tsx
+import React, { useState } from 'react';
+import { Box, Button as MuiButton, Card, CardContent, TextField, Typography } from '@mui/material';
 
-import React from 'react';
-import Button from '../common/Button';
+interface Post {
+    id: number;
+    title: string;
+    content: string;
+}
+
+const initialPosts: Post[] = [
+    { id: 1, title: 'First Post', content: 'This is the first post.' },
+    { id: 2, title: 'Second Post', content: 'This is the second post.' },
+    // Add more initial posts if you want...
+];
 
 const CommunityPage: React.FC = () => {
+    const [posts, setPosts] = useState(initialPosts);
+    const [newPostTitle, setNewPostTitle] = useState('');
+    const [newPostContent, setNewPostContent] = useState('');
+
+    const handlePost = () => {
+        if (newPostTitle && newPostContent) {
+            // Add the new post to the top of the list
+            const newPost: Post = { id: Date.now(), title: newPostTitle, content: newPostContent };
+            setPosts([newPost, ...posts]);
+
+            // Clear the form fields
+            setNewPostTitle('');
+            setNewPostContent('');
+        }
+    };
+
     return (
-        <div>
-            <h1>Community Page</h1>
-            <Button label="Post" />
-            {/* Other components... */}
-        </div>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
+            <Typography variant="h3">Community Page</Typography>
+
+            <TextField
+                variant="outlined"
+                label="New Post Title"
+                value={newPostTitle}
+                onChange={(e) => setNewPostTitle(e.target.value)}
+            />
+            <TextField
+                variant="outlined"
+                label="New Post Content"
+                value={newPostContent}
+                onChange={(e) => setNewPostContent(e.target.value)}
+                multiline
+                rows={4}
+            />
+            <MuiButton variant="contained" color="primary" onClick={handlePost}>Post</MuiButton>
+
+            {posts.map((post) => (
+                <Card key={post.id} sx={{ width: '80%', marginTop: '20px' }}>
+                    <CardContent>
+                        <Typography variant="h5">{post.title}</Typography>
+                        <Typography variant="body2">{post.content}</Typography>
+                    </CardContent>
+                </Card>
+            ))}
+        </Box>
     );
 };
 
